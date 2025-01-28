f0d15c6f5c7e8c3905c050f60c165f2729a1e0ff
NicolasTardy <nicolas.tardy1@gmail.com>
2025-01-28 21:38:46 +0100

    Remplacement complet du projet

--- src/pages/conditions.js
+++ src/pages/conditions.js
@@ -17,18 +17,19 @@ export default function Conditions({ language }) {
       <div className="flex-1 max-w-4xl mx-auto bg-white p-8 rounded-3xl shadow-lg hover:shadow-2xl transition duration-300">
         <p className="text-gray-700 text-lg leading-relaxed mb-6">
           {language === 'fr'
-            ? "Bienvenue dans nos conditions de service. En utilisant notre plateforme, vous acceptez les termes et conditions suivants..."
-            : "Welcome to our Terms of Service. By using our platform, you agree to the following terms and conditions..."}
+            ? "Bienvenue dans les conditions de service de Hand-Secrets. En utilisant notre plateforme, vous acceptez les termes et conditions suivants. Nous tenons à vous informer que nous ne conservons aucune donnée personnelle. Toutes les informations que vous fournissez sont utilisées uniquement pour fournir nos services et ne sont pas stockées sur nos serveurs."
+            : "Welcome to Hand-Secrets' Terms of Service. By using our platform, you agree to the following terms and conditions. We want to inform you that we do not retain any personal data. All information you provide is used solely to deliver our services and is not stored on our servers."}
         </p>
         {/* Ajoutez ici les détails des conditions de service */}
       </div>
 
       {/* Bouton Retour */}
       <div className="flex justify-center p-8">
-        <Link href="/">
-          <a className="text-indigo-600 hover:text-indigo-800 text-lg font-semibold underline">
-            {language === 'fr' ? 'Retour à l\'accueil' : 'Back to Home'}
-          </a>
+        <Link
+          href="/"
+          className="text-indigo-600 hover:text-indigo-800 text-lg font-semibold underline"
+        >
+          {language === 'fr' ? 'Retour à l\'accueil' : 'Back to Home'}
         </Link>
       </div>
     </div>
